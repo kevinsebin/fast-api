480f13a316fc2bbad2d2a8dc6d25012a4e68d2d2
kevinsebin <73955413+kevinsebin@users.noreply.github.com>
2021-10-30 11:47:34 +0530

    Add files via upload

---
+++ app_prog_inter.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Path
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/add/{first}+{second}')
+def add(first, second):
+    sum = int(first) + int(second)
+    return {'sum' : sum}
+
+@app.get('/percent/{first}/{second}')
+def percent(first, second):
+    percent = (int(first) / int(second)) * 100
+    return {'percent' : percent}
+
+
+uvicorn.run(app)
